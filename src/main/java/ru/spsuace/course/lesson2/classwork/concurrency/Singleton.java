0c4600d6a6f108692aa76e1f2f8a0152814b6210
alexander.galkin <alexander.galkin@corp.mail.ru>
2019-04-14 11:55:11 +0300

    fix in singleton

--- src/main/java/ru/spsuace/course/lesson2/classwork/concurrency/Singleton.java
+++ src/main/java/ru/spsuace/course/lesson2/classwork/concurrency/Singleton.java
@@ -15,17 +15,6 @@ public class Singleton {
         return INSTANCE;
     }
 
-//    public static Singleton getInstance() {
-//        if (INSTANCE == null) {
-//            synchronized (Singleton.class) {
-//                if (INSTANCE == null) {
-//                    INSTANCE = new Singleton();
-//                }
-//            }
-//        }
-//        return INSTANCE;
-//    }
-
     private int x = 1;
 
     private Singleton() {}
