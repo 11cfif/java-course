eb60ddbddfa90e70353423f0f81448767027986b
sildvauresberg <16000198@edu.spbgasu.ru>
2019-09-14 22:55:11 +0300

    Add Main#main that prints "Hello world!"

---
+++ src/main/java/ru/cfif11/spsuace/helloworld/Main.java
@@ -0,0 +1,11 @@
+package ru.cfif11.spsuace.helloworld;
+
+public class Main {
+	public static void main(String... args) {
+		System.out.println(hello());
+	}
+	
+	private static String hello() {
+		return "Hello world!";
+	}
+}
